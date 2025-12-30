f9242c95ed398840d9c36ec75427d6f4ba0682df
Nima Golshani <nm.golshani@gmail.com>
2025-12-30 11:40:23 -0500

    changes with errors

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -1,3 +1,5 @@
+# Custom user model
+AUTH_USER_MODEL = 'octofit_tracker.User'
 """
 Django settings for octofit_tracker project.
 
@@ -25,7 +27,11 @@ SECRET_KEY = 'django-insecure-^e6by4&si@&me*x3!9k9zh1lc)5u=xzu4#zjrgvh&75p!o0&oe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME', '')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
